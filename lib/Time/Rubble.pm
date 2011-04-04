e9312105221a92b6294bfd737c2ae1faf2dd878e
Ryo Miyake <ryo.studiom@gmail.com>
2011-04-05 00:16:24 +0900

    created

---
+++ lib/Time/Rubble.pm
@@ -0,0 +1,40 @@
+package Time::Rubble;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+sub new {
+    my ($class) = @_;
+    bless {}, $class;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Time::Rubble - 
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=over 4
+
+=item new
+
+=back
+
+=head1 AUTHOR
+
+Ryo Miyake E<lt>ryo.studiom {at} gmail.comE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/01_new.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+
+use Time::Rubble;
+
+ok my $t = Time::Rubble->new, 'instantiation';
+
+done_testing;
